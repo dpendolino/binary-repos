148ebfd453bb4a2b36b9c4346996e15ee3276828
Daniel Pendolino <daniel@counterhack.com>
2018-03-05 15:33:55 -0500

    formatting

--- trigger-build-darch-recipes.js
+++ trigger-build-darch-recipes.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
+
+/*
+Source: https://hiddentao.com/archives/2016/08/29/triggering-travis-ci-build-from-another-projects-build/
+ */
+
 "use strict";
 
 const shell = require('shelljs'),
@@ -14,7 +19,7 @@ const gitCommitRet = shell.exec('git rev-parse HEAD', {
 
 if (0 !== gitCommitRet.code) {
   console.error('Error getting git commit hash');
-  
+
   process.exit(-1);
 }
 
@@ -26,24 +31,24 @@ console.log('Calling Travis...');
 
 
 got.post(`https://api.travis-ci.org/repo/dpendolino%2Fdarch-recipes/requests`, {
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-    "Travis-API-Version": "3",
-    "Authorization": `token ${process.env.TRAVIS_API_TOKEN}`,
-  },
-  body: JSON.stringify({
-    request: {
-      message: `Trigger build at dpendolino/darch-recipes commit: ${gitCommitHash}`,
-      branch: 'develop',
-    },    
-  }),
-})
-.then(() => {
-  console.log("Triggered build of dpendolino/darch-recipes");
-})
-.catch((err) => {
-  console.error(err);
-  
-  process.exit(-1);
-});
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+      "Travis-API-Version": "3",
+      "Authorization": `token ${process.env.TRAVIS_API_TOKEN}`,
+    },
+    body: JSON.stringify({
+      request: {
+        message: `Trigger build at dpendolino/darch-recipes commit: ${gitCommitHash}`,
+        branch: 'develop',
+      },
+    }),
+  })
+  .then(() => {
+    console.log("Triggered build of dpendolino/darch-recipes");
+  })
+  .catch((err) => {
+    console.error(err);
+
+    process.exit(-1);
+  });
